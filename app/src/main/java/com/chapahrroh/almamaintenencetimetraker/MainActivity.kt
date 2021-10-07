ac4241b7b93919595c4820b2d0f6790b3deec07a
Chapahrroh <chapahrroh@gmail.com>
2021-10-07 17:32:01 -0300

    prueba de nulls

--- app/src/main/java/com/chapahrroh/almamaintenencetimetraker/MainActivity.kt
+++ app/src/main/java/com/chapahrroh/almamaintenencetimetraker/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // leccion 8
+        nullSafety()
+
+    }
+    fun nullSafety()
+    {
+        var myString = "Chapa"
+        println(myString)
     }
 }
